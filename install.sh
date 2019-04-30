79ad3eeb8b03491851f3e68ff967341146974630
hushicai <bluthcy@gmail.com>
2019-04-30 17:40:43 +0800

    split vimrc

--- install.sh
+++ install.sh
@@ -3,15 +3,8 @@ set -e
 
 cd ~/.vim_runtime
 
-echo 'set runtimepath+=~/.vim_runtime
-source ~/.vim_runtime/config/basic.vim
-source ~/.vim_runtime/config/helpers.vim
-source ~/.vim_runtime/config/advance.vim
-source ~/.vim_runtime/config/filetypes.vim
-source ~/.vim_runtime/config/plugins.vim
-' > ~/.vimrc
-
 mkdir -p ~/.config/nvim
-cp -f ~/.vimrc ~/.config/nvim/init.vim
+cp -f ./vimrc ~/.vimrc
+cp -f ./vimrc ~/.config/nvim/init.vim
 
 echo "Installed the Vim configuration successfully!"
---
+++ vimrc
@@ -0,0 +1,8 @@
+" (neo)vim configuration
+
+set runtimepath+=~/.vim_runtime
+source ~/.vim_runtime/config/basic.vim
+source ~/.vim_runtime/config/helpers.vim
+source ~/.vim_runtime/config/advance.vim
+source ~/.vim_runtime/config/filetypes.vim
+source ~/.vim_runtime/config/plugins.vim
